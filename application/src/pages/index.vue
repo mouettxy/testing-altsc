29be7bb9a99dd984ecb16e8fe11a0b24f1c7dccb
Nick <lis@chaikovskie.com>
2022-01-19 07:59:41 +0300

    feat(news): add create template

--- application/src/pages/index.vue
+++ application/src/pages/index.vue
@@ -2,6 +2,22 @@
 
 <template>
   <div class="container px-8 xl:px-0">
+    <router-link
+      v-slot="{ href, navigate }"
+      :to="{ name: 'newsItemCreate' }"
+      custom
+    >
+      <u-btn
+        link
+        :href="href"
+        variant="primary"
+        class="w-full block text-center"
+        @click="navigate"
+      >
+        Создать новость
+      </u-btn>
+    </router-link>
+
     <news-list />
   </div>
 </template>
---
+++ application/src/pages/news/create.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const model = reactive({
+  title: '',
+  description: '',
+})
+</script>
+
+<template>
+  <div class="px-8 xl:px-0">
+    <u-input v-model="model.title" label="Заголовок" />
+    <u-textarea v-model="model.description" rows="8" label="Контент" />
+    <u-btn variant="primary" class="w-full">Создать новость</u-btn>
+  </div>
+</template>
+
+<route lang="yaml">
+name: newsItemCreate
+meta:
+  heading: Создание новости
+  layout: default
+</route>
