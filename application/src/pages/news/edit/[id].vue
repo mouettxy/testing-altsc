80f40ea9bf410578d04ce9d576d4d6baa52b2a5f
Nick <lis@chaikovskie.com>
2022-01-19 11:34:20 +0300

    feat(news): add update logic

--- application/src/pages/news/edit/[id].vue
+++ application/src/pages/news/edit/[id].vue
@@ -1,17 +1,64 @@
 <script setup lang="ts">
+import { useMutation, useQuery, useQueryClient } from 'vue-query'
+import type { NewsEntryUpdate } from '~/api/news'
+import { getNewsEntry, updateNewsEntry } from '~/api/news'
+
+const router = useRouter()
 const route = useRoute()
 
 const model = reactive({
-  title: 'Выбранный нами инновационный путь оправдал надежды граждан',
-  description:
-    'Предварительные выводы неутешительны: сложившаяся структура организации представляет собой интересный эксперимент проверки поэтапного и последовательного развития общества. С учётом сложившейся международной обстановки, экономическая повестка сегодняшнего дня требует от нас анализа новых принципов формирования материально-технической и кадровой базы. Идейные соображения высшего порядка, а также высокотехнологичная концепция общественного уклада способствует повышению качества переосмысления внешнеэкономических политик.',
+  title: '',
+  description: '',
 })
+
+const queryClient = useQueryClient()
+
+useQuery(
+  ['news', { _id: route.params.id }],
+  () => getNewsEntry(route.params.id as string),
+  {
+    onSuccess(response) {
+      if (response.data) {
+        model.title = response.data.title
+        model.description = response.data.description
+      }
+    },
+  }
+)
+
+const { isLoading, mutate } = useMutation(
+  (payload: NewsEntryUpdate) =>
+    updateNewsEntry(route.params.id as string, payload),
+  {
+    onSuccess(response) {
+      queryClient.setQueryData(
+        ['news', { _id: response.data._id }],
+        response.data
+      )
+
+      router.push({ name: 'index' })
+    },
+  }
+)
+
+const handleUpdate = () => {
+  mutate(model)
+}
 </script>
 
 <template>
   <div class="px-8 xl:px-0">
     <u-input v-model="model.title" label="Заголовок" />
     <u-textarea v-model="model.description" rows="8" label="Контент" />
+
+    <u-btn
+      variant="primary"
+      :disabled="isLoading"
+      class="w-full"
+      @click="handleUpdate"
+    >
+      Создать новость
+    </u-btn>
   </div>
 </template>
 
