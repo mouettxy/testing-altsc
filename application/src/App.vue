e8f7d8e618a39ed098b376d00ba52a7f97ffaa78
Nick <lis@chaikovskie.com>
2022-01-19 10:20:22 +0300

    feat: add vue-query provider

--- application/src/App.vue
+++ application/src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import { useQueryProvider } from 'vue-query'
+
 useHead({
   title: 'Новостной портал',
   meta: [{ name: 'description', content: 'Описание новостного портала' }],
 })
+
+useQueryProvider()
 </script>
 
 <template>
