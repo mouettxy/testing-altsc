5d603174845f930c379f7ec73d63e413f782ed71
Nick <lis@chaikovskie.com>
2022-01-19 10:20:57 +0300

    feat(login): implement server logic

--- application/src/pages/login.vue
+++ application/src/pages/login.vue
@@ -1,7 +1,37 @@
 <script setup lang="ts">
+import { useMutation } from 'vue-query'
 import { useHeading } from '~/composables/useHeading'
+import type { UserLoginPayload } from '~/api/auth'
+import { userLogin } from '~/api/auth'
+import { useUserStore } from '~/stores/user'
 
 const heading = useHeading()
+const userStore = useUserStore()
+const router = useRouter()
+
+const { isLoading, isError, error, isSuccess, data, mutate } = useMutation(
+  (payload: UserLoginPayload) => userLogin(payload)
+)
+
+const model = reactive({
+  email: '',
+  password: '',
+})
+
+function handleLogin() {
+  mutate(model)
+}
+
+watch([isSuccess], (value) => {
+  if (value) {
+    model.email = ''
+    model.password = ''
+
+    userStore.loginUser(data.value?.data)
+
+    router.push({ name: 'index' })
+  }
+})
 </script>
 
 <template>
@@ -9,9 +39,24 @@ const heading = useHeading()
     <u-heading variant="h1" class="text-center mb-4" :title="heading" />
     <div class="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
       <div class="px-5 py-7">
-        <u-input label="Почта" />
-        <u-input label="Пароль" type="password" />
-        <u-btn variant="primary" class="w-full">Войти</u-btn>
+        <p
+          v-if="isError"
+          class="text-black bg-orange-200 px-4 py-2 rounded-md mb-4"
+        >
+          {{ error }}
+        </p>
+
+        <u-input v-model="model.email" label="Почта" />
+        <u-input v-model="model.password" label="Пароль" type="password" />
+
+        <u-btn
+          variant="primary"
+          :disabled="isLoading"
+          class="w-full"
+          @click="handleLogin"
+        >
+          Войти
+        </u-btn>
       </div>
 
       <div class="p-4">
