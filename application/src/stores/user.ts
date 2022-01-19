b7ff203e962f5812f933c8e23977c11d3a73f0ba
Nick <lis@chaikovskie.com>
2022-01-19 04:02:17 +0300

    feat(user): update user store with needed logic

--- application/src/stores/user.ts
+++ application/src/stores/user.ts
@@ -1,35 +1,35 @@
 import { acceptHMRUpdate, defineStore } from 'pinia'
+import type { Ref } from 'vue'
+import type { User } from '~/api/auth'
 
 export const useUserStore = defineStore('user', () => {
-  /**
-   * Current named of the user.
-   */
-  const savedName = ref('')
-  const previousNames = ref(new Set<string>())
+  const user: Ref<User | null> = ref(null)
 
-  const usedNames = computed(() => Array.from(previousNames.value))
-  const otherNames = computed(() =>
-    usedNames.value.filter((name) => name !== savedName.value)
-  )
-
-  /**
-   * Changes the current name of the user and saves the one that was used
-   * before.
-   *
-   * @param name - new name to set
-   */
-  function setNewName(name: string) {
-    if (savedName.value) {
-      previousNames.value.add(savedName.value)
-    }
-
-    savedName.value = name
+  function loginUser(loggedUser: User) {
+    user.value = loggedUser
   }
 
+  function logoutUser() {
+    user.value = null
+  }
+
+  const isLoggedIn = computed(() => {
+    return user.value && user.value.token
+  })
+
+  const token = computed(() => {
+    if (!user.value) {
+      return ''
+    }
+
+    return user.value.token
+  })
+
   return {
-    setNewName,
-    otherNames,
-    savedName,
+    loginUser,
+    logoutUser,
+    isLoggedIn,
+    token,
   }
 })
 
