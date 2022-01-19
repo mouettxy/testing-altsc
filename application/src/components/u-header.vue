ad76e03126339ba7859164f6e43d7849e7f22574
Nick <lis@chaikovskie.com>
2022-01-19 08:08:07 +0300

    fix(u-header): fix back button display on smaller screens

--- application/src/components/u-header.vue
+++ application/src/components/u-header.vue
@@ -19,30 +19,35 @@ const route = useRoute()
         md:flex-row md:items-center
       "
     >
-      <u-btn
-        v-if="route.name !== 'index'"
-        class="mt-0.5 mr-2"
-        variant="icon"
-        @click="router.back()"
-      >
-        <svg
-          class="w-6 h-6 block"
-          aria-hidden="true"
-          role="img"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 24 24"
+      <div class="flex relative justify-center md:justify-start">
+        <u-btn
+          v-if="route.name !== 'index'"
+          class="
+            absolute
+            left-0
+            top-1/2
+            transform
+            -translate-y-1/2
+            md:static md:translate-y-0 md:top-0 md:mt-0.5
+          "
+          variant="icon"
+          @click="router.back()"
         >
-          <path
-            d="M15.41 16.58L10.83 12l4.58-4.59L14 6l-6 6l6 6l1.41-1.42z"
-            fill="currentColor"
-          />
-        </svg>
-      </u-btn>
-      <u-heading
-        varaint="h1"
-        class="text-center md:text-left"
-        :title="heading"
-      />
+          <svg
+            class="w-6 h-6 block"
+            aria-hidden="true"
+            role="img"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M15.41 16.58L10.83 12l4.58-4.59L14 6l-6 6l6 6l1.41-1.42z"
+              fill="currentColor"
+            />
+          </svg>
+        </u-btn>
+        <u-heading varaint="h1" :title="heading" />
+      </div>
       <div class="mx-auto space-x-2 md:ml-auto md:mr-0">
         <router-link v-slot="{ href, navigate }" :to="{ name: 'login' }" custom>
           <u-btn variant="primary" link :href="href" @click="navigate">
