60edeab142c948112e10579eb70ab2cd9a93ff41
Nick <lis@chaikovskie.com>
2022-01-19 07:35:11 +0300

    feat(news): add view by id template

--- application/src/layouts/default.vue
+++ application/src/layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="bg-gray-50 min-h-screen">
     <u-header />
     <main class="py-2 flex justify-center">
-      <div class="max-w-screen-xl">
+      <div class="max-w-screen-xl w-full">
         <router-view />
       </div>
     </main>
