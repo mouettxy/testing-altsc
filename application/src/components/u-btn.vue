ebd4911e3d346d85a45b282efa18803cf24ac6f1
Nick <lis@chaikovskie.com>
2022-01-19 05:37:39 +0300

    feat(u-btn): init component

---
+++ application/src/components/u-btn.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+const props = defineProps({
+  variant: {
+    required: false,
+    type: String as PropType<'primary' | 'text'>,
+    default: 'primary',
+    validator: (v: string) => ['primary', 'text'].includes(v),
+  },
+
+  link: {
+    required: false,
+    type: Boolean,
+  },
+})
+
+const btnVariants = {
+  primary: [
+    'bg-blue-500',
+    'hover:bg-blue-600',
+    'focus:bg-blue-700',
+    'focus:shadow-sm',
+    'focus:ring',
+    'focus:ring-blue-500',
+    'focus:ring-opacity-50',
+    'text-white',
+    'shadow-sm',
+    'hover:shadow-md',
+    'font-semibold',
+  ],
+  text: [
+    'text-gray-500',
+    'hover:bg-gray-100',
+    'focus:outline-none',
+    'focus:bg-gray-200',
+    'focus:ring',
+    'focus:ring-gray-400',
+    'focus:ring-opacity-50',
+  ],
+}
+
+const btnAttrs = computed(() => {
+  return {
+    class: btnVariants[props.variant],
+  }
+})
+
+const btnComponent = computed(() => {
+  return props.link ? 'a' : 'button'
+})
+</script>
+
+<template>
+  <component
+    :is="btnComponent"
+    class="
+      transition
+      duration-150
+      px-6
+      py-2
+      cursor-pointer
+      font-normal
+      text-sm
+      rounded-lg
+    "
+    v-bind="btnAttrs"
+  >
+    <slot />
+  </component>
+</template>
