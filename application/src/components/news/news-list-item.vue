917598daf20e1a999287c2a2c230ad43c74795b3
Nick <lis@chaikovskie.com>
2022-01-19 07:55:51 +0300

    feat(news): add news edit template

--- application/src/components/news/news-list-item.vue
+++ application/src/components/news/news-list-item.vue
@@ -32,6 +32,43 @@ defineProps({
         flex flex-col
       "
     >
+      <div class="ml-auto flex space-x-2">
+        <router-link
+          v-slot="{ href, navigate }"
+          :to="{ name: 'newsItemEdit', params: { id: newsId } }"
+          custom
+        >
+          <u-btn link :href="href" variant="icon" @click="navigate">
+            <svg
+              aria-hidden="true"
+              class="block w-6 h-6 text-blue-600"
+              role="img"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M20.71 7.04c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.37-.39-1.02-.39-1.41 0l-1.84 1.83l3.75 3.75M3 17.25V21h3.75L17.81 9.93l-3.75-3.75L3 17.25z"
+                fill="currentColor"
+              />
+            </svg>
+          </u-btn>
+        </router-link>
+
+        <u-btn variant="icon">
+          <svg
+            aria-hidden="true"
+            class="block w-6 h-6 text-red-400"
+            role="img"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M9 3v1H4v2h1v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1V4h-5V3H9M7 6h10v13H7V6m2 2v9h2V8H9m4 0v9h2V8h-2z"
+              fill="currentColor"
+            />
+          </svg>
+        </u-btn>
+      </div>
       <u-heading variant="h2" class="text-center" :title="title" />
       <p
         class="
