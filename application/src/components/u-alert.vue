849c53674f4e620c08e4ac36ed2d373fe7f8163e
Nick <lis@chaikovskie.com>
2022-01-19 10:34:17 +0300

    feat(u-alert): init component

---
+++ application/src/components/u-alert.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps({
+  color: {
+    required: false,
+    type: String,
+    default: 'bg-blue-200',
+  },
+})
+</script>
+
+<template>
+  <p class="text-black px-4 py-2 rounded-md mb-4" :class="[color]">
+    <slot />
+  </p>
+</template>
