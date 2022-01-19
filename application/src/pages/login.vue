8bc9263bdc9ace9553551b6402d6e29c56d94319
Nick <lis@chaikovskie.com>
2022-01-19 05:39:20 +0300

    feat(login): add login template

---
+++ application/src/pages/login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="p-8 xs:p-0 mx-auto md:w-full md:max-w-md">
+    <u-heading variant="h1" class="text-center mb-4" title="Авторизация" />
+    <div class="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
+      <div class="px-5 py-7">
+        <u-input label="Почта" />
+        <u-input label="Пароль" type="password" />
+        <u-btn variant="primary" class="w-full">Войти</u-btn>
+      </div>
+
+      <div class="p-4">
+        <router-link
+          v-slot="{ href, navigate }"
+          :to="{ name: 'register' }"
+          custom
+        >
+          <u-btn variant="text" link :href="href" @click="navigate">
+            Регистрация
+          </u-btn>
+        </router-link>
+      </div>
+    </div>
+    <div class="p-4">
+      <div class="flex justify-center">
+        <router-link v-slot="{ href, navigate }" :to="{ name: 'index' }" custom>
+          <u-btn variant="text" link :href="href" @click="navigate">
+            К новостям
+          </u-btn>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<route lang="yaml">
+name: login
+meta:
+  layout: centered
+</route>
